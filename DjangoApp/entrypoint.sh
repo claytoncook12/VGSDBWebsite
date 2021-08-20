c26ae270abeed3d81224672a998b548265b7679f
claytoncook12 <claytoncook12@gmail.com>
2021-08-20 14:25:06 -0400

    comment out superuser command

--- DjangoApp/entrypoint.sh
+++ DjangoApp/entrypoint.sh
@@ -7,7 +7,7 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # CreateSuperUser
-python manage.py createsuperuser --noinput
+# python manage.py createsuperuser --noinput
 
 # Start Server
 gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 5
