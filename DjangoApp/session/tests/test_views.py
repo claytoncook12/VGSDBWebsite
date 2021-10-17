c6c77b80f39652aba734047828d3407316970ad1
claytoncook12 <claytoncook12@gmail.com>
2021-10-17 09:24:06 -0400

    add TestAboutView and TestFullYtSessionList

--- DjangoApp/session/tests/test_views.py
+++ DjangoApp/session/tests/test_views.py
@@ -7,9 +7,20 @@ from django.urls import reverse
 
 @pytest.mark.django_db
 class TestHomeView:
-    def test_shows_home(self):
-        """The Home View shoudl retrun status code 200"""
-        
+    def test_session_home_200(self):
         client = Client()
         response = client.get(reverse('home'))
-        assert response.status_code == 200
+        assert response.status_code == 200, "Get 200 status For session.views.home"
+
+class TestAboutView:
+    def test_session_about_200(self):
+        client = Client()
+        response = client.get(reverse('about'))
+        assert response.status_code == 200, "Get 200 status for session.views.about"
+
+@pytest.mark.django_db
+class TestFullYtSessionList:
+    def test_session_full_yt_session_list_200(self):
+        client = Client()
+        response = client.get(reverse('full_yt_session_list'))
+        assert response.status_code == 200, "Get 200 status for session.views.full_yt_session_list"
