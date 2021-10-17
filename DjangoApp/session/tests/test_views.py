134b9c8c1c4cbd27048d2e488cef0bb2a88bed13
claytoncook12 <claytoncook12@gmail.com>
2021-10-17 09:36:43 -0400

    add TestSessionDetial 200 status test

--- DjangoApp/session/tests/test_views.py
+++ DjangoApp/session/tests/test_views.py
@@ -1,9 +1,12 @@
 # Session/tests/test_views.py
 
 import pytest
+import datetime
 
 from django.test import Client
 from django.urls import reverse
+
+from session.tests import factories
 
 @pytest.mark.django_db
 class TestHomeView:
@@ -24,3 +27,17 @@ class TestFullYtSessionList:
         client = Client()
         response = client.get(reverse('full_yt_session_list'))
         assert response.status_code == 200, "Get 200 status for session.views.full_yt_session_list"
+
+@pytest.mark.django_db
+class TestSessionDetail:
+    def test_session_detail_200(self):
+        
+        obj = factories.SessionFactory(
+            date = datetime.date(2021, 10, 2)
+        )
+        client = Client()
+        response = client.get(reverse('session_detail',
+                              kwargs={'year': 2021, 'month': 10, 'day': 2}))
+        
+        assert response.status_code == 200, "Get 200 status for session.views.session_detail"
+        
