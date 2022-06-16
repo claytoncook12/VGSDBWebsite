05dcd633071a994815b97a2fd91dd1bb465d182f
claytoncook12 <claytoncook12@gmail.com>
2022-06-16 16:10:44 -0400

    remove requests package

--- DjangoApp/core/settings.py
+++ DjangoApp/core/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'request',
     'session',
 ]
 
@@ -51,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'request.middleware.RequestMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
